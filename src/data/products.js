0fdd911402fbf61583f2c51d0bd601aa1854280c
Wevison Ramalho Silva <wevisonramalho@gmail.com>
2022-07-23 18:28:47 -0300

    [ REFACT ] Changing the images on product.

--- src/data/products.js
+++ src/data/products.js
@@ -3,8 +3,7 @@ export default [
     id: "1",
     attributes: {
       __typename: "Product",
-      imageUrl:
-        "https://strapiuploadprovidertodotask.s3.us-east-1.amazonaws.com/truffon-meio-amargo.png?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEDUaCXNhLWVhc3QtMSJIMEYCIQCH5GMi%2BGa1Dg%2F%2F4kf%2BTjdOZs%2FDbf7x6oafda6J2VPhMwIhAO2Uqjdk5FrP8w%2FpYY28JHYV4gxuYTWi04PmyguIyblnKuQCCH4QAhoMMDc1MTUzMTkwNTU5IgyQNgS5h%2BPtH%2BNhPdoqwQJU39cR%2B8lczuhMke9SdfwzK4xL2o6tJEblYOsdovUCqTklQGhmKsT4ap%2BTsSxpxd42Kl470TIU2rBzrpA48x0FW6uPEiJiV5CWek5iPAAlG2O3fe%2B6rgOzCjFhSPVNffDkUzVzjUSMUAOol1M7nhY1SYKkqDyBwANf8kX%2FytSDeILU%2FZpnIBaOWSMBW14heFZ6J9b6zDHPhVp67%2BeXJffHe%2F08ysJa2IcOSUvUtBOxX6XOhSHIV%2F0%2FRPXqRYG60gGdmqbXu3CI3RnHITcScOrJLrNtXqGagZJDzZk4oZhPEIIBMP8Q3w%2BCVz%2BoA%2BJEPXlkFzdMTtkwCtXqC6OL566RFjGUCEo0e3ldv5T%2FaiHP%2F87BOutIRgu%2FnzijpKZp5PS0PtmaoCtWJ33BnAj7sBqAVRuSDCxgYKwnO88y1g0%2B17YwisLxlgY6sgJIRg8U8AokDvq%2FAzi5ZG4lgGjCDMR5Ad%2FO9oeedQg8b2%2BBbSvNkIKz0kCgNnfqH5h%2FZb%2BfvTGHErAJZ6gJ4uhu6%2BGCZYBz7n8w4Jx88DXpx2C1WuINU3HOtmL69WTMusU79KFn4ZZ%2FpYcJhCuEsd8tHRg793LADRkPFfWY2FN%2BUiiHPap0LsnlGcWHSxb%2BHUEjlg9AnXEAo7QL0%2BDxp%2BCLYC3HX%2BrV3a3M53LrDtlbYzXU5Nc5iaUuBWoHImAeDoxtVGD9Po4555coHo62UiCMNzK1BS9pEKRIPLcdF3C59lUagRfCvmDAHN3vQ3Zl7Fc6ym5luWDlI9slOXVKgVyKm2jg0ds4OxenmrqTAuEndQihJnIdbeAiFG0gJCcHrckXO99YY%2BUqiMYBjWqnvvTbSyk%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20220723T211915Z&X-Amz-SignedHeaders=host&X-Amz-Expires=300&X-Amz-Credential=ASIARC732V2PVJLTZC7L%2F20220723%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=2d1f1706eb9d9cc3b8475d13385c6418b0e1bdba9d4e7147deac10d34dba8998",
+      imageUrl: "/images/truffon-meio-amargo.png",
       name: "Trufa BENDITO CACAU 55% CACAU 30 G",
       price: 303,
       sellingPrice: 273,
@@ -15,8 +14,7 @@ export default [
     id: "2",
     attributes: {
       __typename: "Product",
-      imageUrl:
-        "https://strapiuploadprovidertodotask.s3.us-east-1.amazonaws.com/trufa-morango-30g.png?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEDUaCXNhLWVhc3QtMSJIMEYCIQCH5GMi%2BGa1Dg%2F%2F4kf%2BTjdOZs%2FDbf7x6oafda6J2VPhMwIhAO2Uqjdk5FrP8w%2FpYY28JHYV4gxuYTWi04PmyguIyblnKuQCCH4QAhoMMDc1MTUzMTkwNTU5IgyQNgS5h%2BPtH%2BNhPdoqwQJU39cR%2B8lczuhMke9SdfwzK4xL2o6tJEblYOsdovUCqTklQGhmKsT4ap%2BTsSxpxd42Kl470TIU2rBzrpA48x0FW6uPEiJiV5CWek5iPAAlG2O3fe%2B6rgOzCjFhSPVNffDkUzVzjUSMUAOol1M7nhY1SYKkqDyBwANf8kX%2FytSDeILU%2FZpnIBaOWSMBW14heFZ6J9b6zDHPhVp67%2BeXJffHe%2F08ysJa2IcOSUvUtBOxX6XOhSHIV%2F0%2FRPXqRYG60gGdmqbXu3CI3RnHITcScOrJLrNtXqGagZJDzZk4oZhPEIIBMP8Q3w%2BCVz%2BoA%2BJEPXlkFzdMTtkwCtXqC6OL566RFjGUCEo0e3ldv5T%2FaiHP%2F87BOutIRgu%2FnzijpKZp5PS0PtmaoCtWJ33BnAj7sBqAVRuSDCxgYKwnO88y1g0%2B17YwisLxlgY6sgJIRg8U8AokDvq%2FAzi5ZG4lgGjCDMR5Ad%2FO9oeedQg8b2%2BBbSvNkIKz0kCgNnfqH5h%2FZb%2BfvTGHErAJZ6gJ4uhu6%2BGCZYBz7n8w4Jx88DXpx2C1WuINU3HOtmL69WTMusU79KFn4ZZ%2FpYcJhCuEsd8tHRg793LADRkPFfWY2FN%2BUiiHPap0LsnlGcWHSxb%2BHUEjlg9AnXEAo7QL0%2BDxp%2BCLYC3HX%2BrV3a3M53LrDtlbYzXU5Nc5iaUuBWoHImAeDoxtVGD9Po4555coHo62UiCMNzK1BS9pEKRIPLcdF3C59lUagRfCvmDAHN3vQ3Zl7Fc6ym5luWDlI9slOXVKgVyKm2jg0ds4OxenmrqTAuEndQihJnIdbeAiFG0gJCcHrckXO99YY%2BUqiMYBjWqnvvTbSyk%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20220723T211744Z&X-Amz-SignedHeaders=host&X-Amz-Expires=300&X-Amz-Credential=ASIARC732V2PVJLTZC7L%2F20220723%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=9eb08a052b770ea963033ac74847c9fa4482348a0e4bf4c463e7d9f97f31d9f4",
+      imageUrl: "/images/trufa-morango-30g.png",
       name: "TRUFA MORANGO",
       price: 123,
       sellingPrice: 111,
@@ -27,8 +25,7 @@ export default [
     id: "3",
     attributes: {
       __typename: "Product",
-      imageUrl:
-        "https://strapiuploadprovidertodotask.s3.us-east-1.amazonaws.com/trufa-tradicional.png?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEDUaCXNhLWVhc3QtMSJIMEYCIQCH5GMi%2BGa1Dg%2F%2F4kf%2BTjdOZs%2FDbf7x6oafda6J2VPhMwIhAO2Uqjdk5FrP8w%2FpYY28JHYV4gxuYTWi04PmyguIyblnKuQCCH4QAhoMMDc1MTUzMTkwNTU5IgyQNgS5h%2BPtH%2BNhPdoqwQJU39cR%2B8lczuhMke9SdfwzK4xL2o6tJEblYOsdovUCqTklQGhmKsT4ap%2BTsSxpxd42Kl470TIU2rBzrpA48x0FW6uPEiJiV5CWek5iPAAlG2O3fe%2B6rgOzCjFhSPVNffDkUzVzjUSMUAOol1M7nhY1SYKkqDyBwANf8kX%2FytSDeILU%2FZpnIBaOWSMBW14heFZ6J9b6zDHPhVp67%2BeXJffHe%2F08ysJa2IcOSUvUtBOxX6XOhSHIV%2F0%2FRPXqRYG60gGdmqbXu3CI3RnHITcScOrJLrNtXqGagZJDzZk4oZhPEIIBMP8Q3w%2BCVz%2BoA%2BJEPXlkFzdMTtkwCtXqC6OL566RFjGUCEo0e3ldv5T%2FaiHP%2F87BOutIRgu%2FnzijpKZp5PS0PtmaoCtWJ33BnAj7sBqAVRuSDCxgYKwnO88y1g0%2B17YwisLxlgY6sgJIRg8U8AokDvq%2FAzi5ZG4lgGjCDMR5Ad%2FO9oeedQg8b2%2BBbSvNkIKz0kCgNnfqH5h%2FZb%2BfvTGHErAJZ6gJ4uhu6%2BGCZYBz7n8w4Jx88DXpx2C1WuINU3HOtmL69WTMusU79KFn4ZZ%2FpYcJhCuEsd8tHRg793LADRkPFfWY2FN%2BUiiHPap0LsnlGcWHSxb%2BHUEjlg9AnXEAo7QL0%2BDxp%2BCLYC3HX%2BrV3a3M53LrDtlbYzXU5Nc5iaUuBWoHImAeDoxtVGD9Po4555coHo62UiCMNzK1BS9pEKRIPLcdF3C59lUagRfCvmDAHN3vQ3Zl7Fc6ym5luWDlI9slOXVKgVyKm2jg0ds4OxenmrqTAuEndQihJnIdbeAiFG0gJCcHrckXO99YY%2BUqiMYBjWqnvvTbSyk%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20220723T211854Z&X-Amz-SignedHeaders=host&X-Amz-Expires=300&X-Amz-Credential=ASIARC732V2PVJLTZC7L%2F20220723%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=7a0d755412a001a230a192795816693223157a8266781965dc233b25b95b145d",
+      imageUrl: "/images/trufa-tradicional.png",
       name: "TRUFA TRADICIONAL 30 G",
       price: 303,
       sellingPrice: 273,
@@ -39,8 +36,7 @@ export default [
     id: "4",
     attributes: {
       __typename: "Product",
-      imageUrl:
-        "https://strapiuploadprovidertodotask.s3.us-east-1.amazonaws.com/trufa-coco.png?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEDUaCXNhLWVhc3QtMSJIMEYCIQCH5GMi%2BGa1Dg%2F%2F4kf%2BTjdOZs%2FDbf7x6oafda6J2VPhMwIhAO2Uqjdk5FrP8w%2FpYY28JHYV4gxuYTWi04PmyguIyblnKuQCCH4QAhoMMDc1MTUzMTkwNTU5IgyQNgS5h%2BPtH%2BNhPdoqwQJU39cR%2B8lczuhMke9SdfwzK4xL2o6tJEblYOsdovUCqTklQGhmKsT4ap%2BTsSxpxd42Kl470TIU2rBzrpA48x0FW6uPEiJiV5CWek5iPAAlG2O3fe%2B6rgOzCjFhSPVNffDkUzVzjUSMUAOol1M7nhY1SYKkqDyBwANf8kX%2FytSDeILU%2FZpnIBaOWSMBW14heFZ6J9b6zDHPhVp67%2BeXJffHe%2F08ysJa2IcOSUvUtBOxX6XOhSHIV%2F0%2FRPXqRYG60gGdmqbXu3CI3RnHITcScOrJLrNtXqGagZJDzZk4oZhPEIIBMP8Q3w%2BCVz%2BoA%2BJEPXlkFzdMTtkwCtXqC6OL566RFjGUCEo0e3ldv5T%2FaiHP%2F87BOutIRgu%2FnzijpKZp5PS0PtmaoCtWJ33BnAj7sBqAVRuSDCxgYKwnO88y1g0%2B17YwisLxlgY6sgJIRg8U8AokDvq%2FAzi5ZG4lgGjCDMR5Ad%2FO9oeedQg8b2%2BBbSvNkIKz0kCgNnfqH5h%2FZb%2BfvTGHErAJZ6gJ4uhu6%2BGCZYBz7n8w4Jx88DXpx2C1WuINU3HOtmL69WTMusU79KFn4ZZ%2FpYcJhCuEsd8tHRg793LADRkPFfWY2FN%2BUiiHPap0LsnlGcWHSxb%2BHUEjlg9AnXEAo7QL0%2BDxp%2BCLYC3HX%2BrV3a3M53LrDtlbYzXU5Nc5iaUuBWoHImAeDoxtVGD9Po4555coHo62UiCMNzK1BS9pEKRIPLcdF3C59lUagRfCvmDAHN3vQ3Zl7Fc6ym5luWDlI9slOXVKgVyKm2jg0ds4OxenmrqTAuEndQihJnIdbeAiFG0gJCcHrckXO99YY%2BUqiMYBjWqnvvTbSyk%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20220723T211656Z&X-Amz-SignedHeaders=host&X-Amz-Expires=300&X-Amz-Credential=ASIARC732V2PVJLTZC7L%2F20220723%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=833d5ee44e90998c87a6071bd1c086b7047644edd4cf6b3cbe6ad4a88d99b4d4",
+      imageUrl: "/images/trufa-coco.png",
       name: "TRUFA DE COCO 30 G",
       price: 303,
       sellingPrice: 273,
@@ -51,8 +47,7 @@ export default [
     id: "5",
     attributes: {
       __typename: "Product",
-      imageUrl:
-        "https://strapiuploadprovidertodotask.s3.us-east-1.amazonaws.com/trufa-branco.png?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEDUaCXNhLWVhc3QtMSJIMEYCIQCH5GMi%2BGa1Dg%2F%2F4kf%2BTjdOZs%2FDbf7x6oafda6J2VPhMwIhAO2Uqjdk5FrP8w%2FpYY28JHYV4gxuYTWi04PmyguIyblnKuQCCH4QAhoMMDc1MTUzMTkwNTU5IgyQNgS5h%2BPtH%2BNhPdoqwQJU39cR%2B8lczuhMke9SdfwzK4xL2o6tJEblYOsdovUCqTklQGhmKsT4ap%2BTsSxpxd42Kl470TIU2rBzrpA48x0FW6uPEiJiV5CWek5iPAAlG2O3fe%2B6rgOzCjFhSPVNffDkUzVzjUSMUAOol1M7nhY1SYKkqDyBwANf8kX%2FytSDeILU%2FZpnIBaOWSMBW14heFZ6J9b6zDHPhVp67%2BeXJffHe%2F08ysJa2IcOSUvUtBOxX6XOhSHIV%2F0%2FRPXqRYG60gGdmqbXu3CI3RnHITcScOrJLrNtXqGagZJDzZk4oZhPEIIBMP8Q3w%2BCVz%2BoA%2BJEPXlkFzdMTtkwCtXqC6OL566RFjGUCEo0e3ldv5T%2FaiHP%2F87BOutIRgu%2FnzijpKZp5PS0PtmaoCtWJ33BnAj7sBqAVRuSDCxgYKwnO88y1g0%2B17YwisLxlgY6sgJIRg8U8AokDvq%2FAzi5ZG4lgGjCDMR5Ad%2FO9oeedQg8b2%2BBbSvNkIKz0kCgNnfqH5h%2FZb%2BfvTGHErAJZ6gJ4uhu6%2BGCZYBz7n8w4Jx88DXpx2C1WuINU3HOtmL69WTMusU79KFn4ZZ%2FpYcJhCuEsd8tHRg793LADRkPFfWY2FN%2BUiiHPap0LsnlGcWHSxb%2BHUEjlg9AnXEAo7QL0%2BDxp%2BCLYC3HX%2BrV3a3M53LrDtlbYzXU5Nc5iaUuBWoHImAeDoxtVGD9Po4555coHo62UiCMNzK1BS9pEKRIPLcdF3C59lUagRfCvmDAHN3vQ3Zl7Fc6ym5luWDlI9slOXVKgVyKm2jg0ds4OxenmrqTAuEndQihJnIdbeAiFG0gJCcHrckXO99YY%2BUqiMYBjWqnvvTbSyk%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20220723T212054Z&X-Amz-SignedHeaders=host&X-Amz-Expires=300&X-Amz-Credential=ASIARC732V2PVJLTZC7L%2F20220723%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=964ae76da6f665d3f34f3d8952de1559177755e3ac601b30f096758640dc865f",
+      imageUrl: "/images/trufa-branco.png",
       name: "TRUFA BRANCO 30G",
       price: 110,
       sellingPrice: 99,
@@ -63,8 +58,7 @@ export default [
     id: "6",
     attributes: {
       __typename: "Product",
-      imageUrl:
-        "https://strapiuploadprovidertodotask.s3.us-east-1.amazonaws.com/mezzo.png?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEDUaCXNhLWVhc3QtMSJIMEYCIQCH5GMi%2BGa1Dg%2F%2F4kf%2BTjdOZs%2FDbf7x6oafda6J2VPhMwIhAO2Uqjdk5FrP8w%2FpYY28JHYV4gxuYTWi04PmyguIyblnKuQCCH4QAhoMMDc1MTUzMTkwNTU5IgyQNgS5h%2BPtH%2BNhPdoqwQJU39cR%2B8lczuhMke9SdfwzK4xL2o6tJEblYOsdovUCqTklQGhmKsT4ap%2BTsSxpxd42Kl470TIU2rBzrpA48x0FW6uPEiJiV5CWek5iPAAlG2O3fe%2B6rgOzCjFhSPVNffDkUzVzjUSMUAOol1M7nhY1SYKkqDyBwANf8kX%2FytSDeILU%2FZpnIBaOWSMBW14heFZ6J9b6zDHPhVp67%2BeXJffHe%2F08ysJa2IcOSUvUtBOxX6XOhSHIV%2F0%2FRPXqRYG60gGdmqbXu3CI3RnHITcScOrJLrNtXqGagZJDzZk4oZhPEIIBMP8Q3w%2BCVz%2BoA%2BJEPXlkFzdMTtkwCtXqC6OL566RFjGUCEo0e3ldv5T%2FaiHP%2F87BOutIRgu%2FnzijpKZp5PS0PtmaoCtWJ33BnAj7sBqAVRuSDCxgYKwnO88y1g0%2B17YwisLxlgY6sgJIRg8U8AokDvq%2FAzi5ZG4lgGjCDMR5Ad%2FO9oeedQg8b2%2BBbSvNkIKz0kCgNnfqH5h%2FZb%2BfvTGHErAJZ6gJ4uhu6%2BGCZYBz7n8w4Jx88DXpx2C1WuINU3HOtmL69WTMusU79KFn4ZZ%2FpYcJhCuEsd8tHRg793LADRkPFfWY2FN%2BUiiHPap0LsnlGcWHSxb%2BHUEjlg9AnXEAo7QL0%2BDxp%2BCLYC3HX%2BrV3a3M53LrDtlbYzXU5Nc5iaUuBWoHImAeDoxtVGD9Po4555coHo62UiCMNzK1BS9pEKRIPLcdF3C59lUagRfCvmDAHN3vQ3Zl7Fc6ym5luWDlI9slOXVKgVyKm2jg0ds4OxenmrqTAuEndQihJnIdbeAiFG0gJCcHrckXO99YY%2BUqiMYBjWqnvvTbSyk%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20220723T211413Z&X-Amz-SignedHeaders=host&X-Amz-Expires=300&X-Amz-Credential=ASIARC732V2PVJLTZC7L%2F20220723%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=80d666a11ca820b3374b604fb9fce357ab93577da72e654d32920328f3de8a20",
+      imageUrl: "/images/mezzo.png",
       name: "TRUFA MEZZO 30 G",
       price: 303,
       sellingPrice: 273,
@@ -75,8 +69,7 @@ export default [
     id: "7",
     attributes: {
       __typename: "Product",
-      imageUrl:
-        "https://strapiuploadprovidertodotask.s3.us-east-1.amazonaws.com/trufa-morango-maracuja.png?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEDUaCXNhLWVhc3QtMSJIMEYCIQCH5GMi%2BGa1Dg%2F%2F4kf%2BTjdOZs%2FDbf7x6oafda6J2VPhMwIhAO2Uqjdk5FrP8w%2FpYY28JHYV4gxuYTWi04PmyguIyblnKuQCCH4QAhoMMDc1MTUzMTkwNTU5IgyQNgS5h%2BPtH%2BNhPdoqwQJU39cR%2B8lczuhMke9SdfwzK4xL2o6tJEblYOsdovUCqTklQGhmKsT4ap%2BTsSxpxd42Kl470TIU2rBzrpA48x0FW6uPEiJiV5CWek5iPAAlG2O3fe%2B6rgOzCjFhSPVNffDkUzVzjUSMUAOol1M7nhY1SYKkqDyBwANf8kX%2FytSDeILU%2FZpnIBaOWSMBW14heFZ6J9b6zDHPhVp67%2BeXJffHe%2F08ysJa2IcOSUvUtBOxX6XOhSHIV%2F0%2FRPXqRYG60gGdmqbXu3CI3RnHITcScOrJLrNtXqGagZJDzZk4oZhPEIIBMP8Q3w%2BCVz%2BoA%2BJEPXlkFzdMTtkwCtXqC6OL566RFjGUCEo0e3ldv5T%2FaiHP%2F87BOutIRgu%2FnzijpKZp5PS0PtmaoCtWJ33BnAj7sBqAVRuSDCxgYKwnO88y1g0%2B17YwisLxlgY6sgJIRg8U8AokDvq%2FAzi5ZG4lgGjCDMR5Ad%2FO9oeedQg8b2%2BBbSvNkIKz0kCgNnfqH5h%2FZb%2BfvTGHErAJZ6gJ4uhu6%2BGCZYBz7n8w4Jx88DXpx2C1WuINU3HOtmL69WTMusU79KFn4ZZ%2FpYcJhCuEsd8tHRg793LADRkPFfWY2FN%2BUiiHPap0LsnlGcWHSxb%2BHUEjlg9AnXEAo7QL0%2BDxp%2BCLYC3HX%2BrV3a3M53LrDtlbYzXU5Nc5iaUuBWoHImAeDoxtVGD9Po4555coHo62UiCMNzK1BS9pEKRIPLcdF3C59lUagRfCvmDAHN3vQ3Zl7Fc6ym5luWDlI9slOXVKgVyKm2jg0ds4OxenmrqTAuEndQihJnIdbeAiFG0gJCcHrckXO99YY%2BUqiMYBjWqnvvTbSyk%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20220723T211819Z&X-Amz-SignedHeaders=host&X-Amz-Expires=300&X-Amz-Credential=ASIARC732V2PVJLTZC7L%2F20220723%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=0e6b42f9d1ab0d6c644c0217d4f6c6d52751e781bd336c5b2d0f8e7006842cfe",
+      imageUrl: "/images/trufa-morango-maracuja.png",
       name: "TRUFA MORANGO E MARACUJÁ 13,5 G",
       price: 110,
       sellingPrice: 99,
